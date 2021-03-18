cc8b544cb0e447c4e6cf47f64b5c1e482eef6a68
paulsaslocal <paul.sas@btrl.ro>
2021-03-18 04:43:00 +0200

    Step3 TEST5:Add root leaf to Tree - PASS ALL

--- src/Entity/Tree.js
+++ src/Entity/Tree.js
@@ -1,9 +1,17 @@
+import Leaf from "./Leaf";
 
 class Tree {
     constructor() {
-        this.leafs = []
+        const rootLeafData = {
+            ID:0,
+            ParentNode:null
+        }
+        const leaf=new Leaf(rootLeafData);
+        this.leafs=[leaf];
     }
-
+    getRoot = ()=>{
+        return this.leafs[0]
+    }
 
 }
 export  default Tree
