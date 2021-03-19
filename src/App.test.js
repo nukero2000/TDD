0ca11086c6144b002cd15abc4d5fe72d4819c51e
paulsaslocal <paul.sas@btrl.ro>
2021-03-19 06:49:39 +0200

    Step4 test 9 : Grow random tree with n leafs PASS

--- src/App.test.js
+++ src/App.test.js
@@ -3,23 +3,23 @@ import App from './App';
 import Tree from "./Entity/Tree";
 import Leaf from "./Entity/Leaf";
 //TEST 1
-it('renders learn react link', () => {
+it('Test 1: renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 //TEST 2
-it('Create tree',() => {
+it('Test 2: Create tree',() => {
     const tree= new Tree();
     expect(tree.leafs.length).toBe(1)
 })
 //TEST 3
-it('Create one leaf ',()=>{
+it('Test 3: Create one leaf ',()=>{
     const leaf = new Leaf();
     expect(leaf).toBeDefined();
 })
 //TEST 4
-it('Create leaf for root node on tree', ()=>{
+it('Test 4: Create leaf for root node on tree', ()=>{
     const leafData = {
         ID:0,
         ParentNode:null
@@ -28,7 +28,7 @@ it('Create leaf for root node on tree', ()=>{
     expect(leaf).toMatchObject(leafData)
 })
 //TEST 5
-it('Add root leaf to Tree', ()=>{
+it('Test 5: Add root leaf to Tree', ()=>{
     const tree= new Tree();
     const leafData = {
         ID:0,
@@ -37,7 +37,7 @@ it('Add root leaf to Tree', ()=>{
     expect(tree.getRoot()).toMatchObject(leafData)
 })
 //TEST 6
-it('Grow branch from leaf',()=>{
+it('Test 6: Grow branch from leaf',()=>{
     const leaf1Data = {
         ID:1,
         ParentNode:null
@@ -49,24 +49,25 @@ it('Grow branch from leaf',()=>{
     expect(leaf1.branches).toContainEqual(leaf2);
 })
 // TEST 7
-it('Grow branches from tree leaf', () => {
+it('Test 7: Grow branches from tree leaf', () => {
     const tree= new Tree();
     tree.growBranches(tree.getRoot(),3);
-    expect(tree.countLeafs()).toBe(4);
+    expect(tree.countLeafs).toBe(4);
 })
 // TEST 8
-it('Grow a custom binary tree on 3 level', () => {
+it('Test 8: Grow a custom binary tree on 3 level', () => {
     const tree = new Tree();
     tree.growBranches(tree.getRoot(),2)
     let branch;
     for(branch of tree.getRoot().branches){
         tree.growBranches(branch,2)
     }
-    expect(tree.countLeafs()).toBe(7);
+    expect(tree.countLeafs).toBe(7);
 })
 //TEST 9
-fit('Grow random tree with n leafs', ()=>{
+it('Test 9: Grow random tree with n leafs', ()=>{
     const tree = new Tree();
     tree.grow(10,3);
-    expect(tree.countLeafs()).toBe(7);
+    expect(tree.countLeafs).toBe(10);
 })
+
--- src/Entity/Tree.js
+++ src/Entity/Tree.js
@@ -12,14 +12,39 @@ class Tree {
     getRoot = () => {
         return this.leafs[0]
     }
-    countLeafs = () => {
+    get countLeafs(){
         return this.leafs.length;
     }
     growBranches = (leaf,nrOfBranches) => {
         for (let i = 0;i<nrOfBranches;i++){
-           let newLeaf= leaf.growBranch(this.countLeafs())
+           let newLeaf= leaf.growBranch(this.countLeafs)
            this.leafs.push(newLeaf);
         }
+    }
+    randomNumber = (min, max) => {
+        return Math.floor(Math.random()*(max-min+1)+min)
+    }
+    grow = (leafsNumber,maxLeafsPerBranch,level,nextLevel) => {
+        if (this.countLeafs>=leafsNumber) return;
+
+        nextLevel = typeof nextLevel === 'undefined' ? []:nextLevel
+        level = Array.isArray(level) ? level : [this.getRoot()];
+        maxLeafsPerBranch = maxLeafsPerBranch < leafsNumber-this.countLeafs?
+               maxLeafsPerBranch:leafsNumber-this.countLeafs;
+
+        let randomLeafNumber = this.randomNumber(0,level.length-1);
+        let leaf = level[randomLeafNumber];
+        level.splice(randomLeafNumber, 1)
+        this.growBranches(leaf,this.randomNumber(1,maxLeafsPerBranch));
+        nextLevel=nextLevel.concat(leaf.branches);
+        if (this.countLeafs<leafsNumber) {
+            if (level.length===0){
+                level=nextLevel;
+                nextLevel=[];
+            }
+        this.grow(leafsNumber,maxLeafsPerBranch,level,nextLevel)
+       }
+
     }
 
 }
